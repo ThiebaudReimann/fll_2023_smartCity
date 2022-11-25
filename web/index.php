95cd0c9c921c33f6d5ee7b1316189554bab9ecab
Manuel Selch <manuel@selch.de>
2022-11-25 11:30:04 +0100

    web sql connection

--- web/index.php
+++ web/index.php
@@ -18,8 +18,8 @@
         $conn = mysqli_connect($servername, $username, $password);
 
         // Check connection
-        if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
         } 
         echo "Connected successfully";
     }
